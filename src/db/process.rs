b819585ea069a39327564a1b3c507ffde1ae11ab
POPPIN-FUMI <f.kawasaki@elsoul.nl>
2022-06-17 12:06:15 +0200

    add spinner

--- src/db/process.rs
+++ src/db/process.rs
@@ -2,12 +2,14 @@ use tokio::process::Command;
 use crate::style_print::*;
 use regex::Regex;
 use std::str;
+use spinners::{Spinner, Spinners};
 
 fn regex(re_str: &str) -> Regex {
     Regex::new(re_str).unwrap()
 }
 
 pub async fn process_db_migrate() {
+    let mut sp = Spinner::new(Spinners::Aesthetic, "Migrating..".into());
     let output = Command::new("sea-orm-cli")
         .args(&[
         "migrate",
@@ -21,15 +23,20 @@ pub async fn process_db_migrate() {
             let rt = regex("Running");
             match rt.is_match(err) {
                 true => {
+                    sp.stop();
                     log_success("Successfully DB Migrated!").await;
                 }
                 false => {
+                    sp.stop();
                     panic!("{:?}", err)
                 }
               }
             }
-        Err(err) => panic!("error = {:?}", err)
+        Err(err) => {
+            sp.stop();
+            panic!("error = {:?}", err)
         }
+    }
 }
 
 pub async fn process_db_reset() {
